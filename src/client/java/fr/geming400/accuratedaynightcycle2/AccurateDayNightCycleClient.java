f869a21d8518f83ce565e368c5aac82a11cb2fad
Geming400 <Geming400@gmail.com>
2026-02-10 18:49:40 +0100

    Initial commit

---
+++ src/client/java/fr/geming400/accuratedaynightcycle2/AccurateDayNightCycleClient.java
@@ -0,0 +1,10 @@
+package fr.geming400.accuratedaynightcycle2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AccurateDayNightCycleClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
