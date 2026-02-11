1196298cc51245ef29706e269a8efa88bec15f32
Geming400 <Geming400@gmail.com>
2026-02-11 13:26:00 +0100

    Added iris warning

--- src/client/java/fr/geming400/accuratedaynightcycle2/AccurateDayNightCycleClient.java
+++ src/client/java/fr/geming400/accuratedaynightcycle2/AccurateDayNightCycleClient.java
@@ -1,10 +1,33 @@
 package fr.geming400.accuratedaynightcycle2;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 public class AccurateDayNightCycleClient implements ClientModInitializer {
+	private static boolean warnAboutIris = false;
+
+	public static void warnAboutIris() {
+		ClientPlayerEntity player = MinecraftClient.getInstance().player;
+		if (player == null) return;
+		if (warnAboutIris || AccurateDayNightCycle.CONFIG.accurateCelestialBodies()) {
+			warnAboutIris = false;
+			player.sendMessage(Text.translatable("acdn.warning.iris", Text.translatable("text.config.adnc-config.option.accurateCelestialBodies")).formatted(Formatting.GOLD));
+		}
+	}
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		AccurateDayNightCycle.CONFIG.subscribeToAccurateCelestialBodies((value) -> {
+			ClientPlayerEntity player = MinecraftClient.getInstance().player;
+			if (FabricLoader.getInstance().isModLoaded("iris") && value == true) {
+				warnAboutIris = true;
+				if (player != null)
+					warnAboutIris();
+			}
+		});
 	}
 }
---
+++ src/client/java/fr/geming400/accuratedaynightcycle2/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -0,0 +1,17 @@
+package fr.geming400.accuratedaynightcycle2.mixin.client;
+
+import fr.geming400.accuratedaynightcycle2.AccurateDayNightCycleClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
+abstract class ClientPlayNetworkHandlerMixin {
+    @Inject(at = @At("TAIL"), method = "onGameJoin")
+    private void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
+        AccurateDayNightCycleClient.warnAboutIris();
+    }
+}
